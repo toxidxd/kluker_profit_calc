d1c2fe8c892d184cad2a1be70a1293ddef0835b1
toxid <toxid13@yandex.ru>
2024-10-18 23:22:10 +0300

    opencv probe

--- opencv_probe.py
+++ opencv_probe.py
@@ -1,24 +1,27 @@
 import cv2
 import numpy as np
-
 # Reading the main image
-rgb_img = cv2.imread('image.jpg', 1)
+img_rgb = cv2.imread(r'image.jpg',1)
 # It is need to be convert it to grayscale
-gray_img = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2GRAY)
-# Reading the template image
-template = cv2.imread('template_1.jpg', 0)
+img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+# Read the template
+template = cv2.imread(r'template_11.jpg',0)
 # Store width in variable w and height in variable h of template
-w, h = template.shape[:-1]
+print(template.shape)
+w, h = template.shape
 # Now we perform match operations.
-res = cv2.matchTemplate(gray_img, template, cv2.TM_CCOEFF_NORMED)
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
 # Declare a threshold
 threshold = 0.8
-# Store the coordinates of matched location in a numpy array
-loc = np.where(res >= threshold)
-# Draw the rectangle around the matched region.
+# Store the coordinates of matched region in a numpy array
+loc = np.where( res >= threshold)
+print(loc)
+# Draw a rectangle around the matched region.
 for pt in zip(*loc[::-1]):
-    cv2.rectangle(rgb_img, pt, (pt[0] + w, pt[1] + h), (0, 255, 255), 2)
+ print(pt)
+ print(pt[0] + w, pt[1] + h)
+ cv2.rectangle(img_rgb, pt,(pt[0] + w, pt[1] + h),(0,255,255), 2)
 # Now display the final matched template image
-# cv2.imshow('Detected', rgb_img)
-
-
+# cv2.imshow('Detected',img_rgb)
+# input()
+cv2.imwrite('cvtest.jpg', img_rgb)
