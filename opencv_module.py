a50a07713f29cc918e7f59e2088670aa1c0e86be
toxidxd <toxid13@yandex.ru>
2024-10-19 17:02:58 +0300

    opencv added

---
+++ opencv_module.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+
+def get_borders(img, template):
+    # Reading the main image
+    rgb_img = cv2.imread(img, 1)
+    w_img = rgb_img.shape[1]
+    # It is need to be convert it to grayscale
+    gray_img = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2GRAY)
+    # Reading the template image
+    template = cv2.imread(template, 0)
+    # Store width in variable w and height in variable h of template
+    w, h = template.shape
+    # Now we perform match operations.
+    res = cv2.matchTemplate(gray_img, template, cv2.TM_CCOEFF_NORMED)
+    # Declare a threshold
+    threshold = 0.8
+    # Store the coordinates of matched location in a numpy array
+    loc = np.where(res >= threshold)
+    print('loc', loc[1][0], loc[0][0])
+    print('loc', loc[1][0], loc[0][0])
+    result = (0, int(loc[0][0]), w_img, int(loc[0][0] + h))
+    print(result)
+    return result
+
+
